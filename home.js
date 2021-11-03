c5db86cc901b89450d681aef2298219c4185ffc2
APIYOJENNIFER <apiyojennifer4@gmail.com>
2021-11-03 22:34:45 +0300

    initial commit

---
+++ home.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("homeSideNav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("homeSideNav").style.width = "0px";
+}
+
+window.onload = function(){
+    document.getElementById("rangerName").innerHTML = "Welcome Jenny";
+};
